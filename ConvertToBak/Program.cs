0fbbabb8b84b5f2b799e6bb7ceee4df6285c9f92
Eriel Miquilino <eriel.pereira@ndd.com.br>
2020-03-09 14:34:28 -0300

    Add project files.

---
+++ ConvertToBak/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConvertToBak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var safeFile = args[0];
+
+            var path = Path.GetDirectoryName(safeFile);
+
+            var command = $"/c safetosql \"{safeFile}\" -output \"{path}\"";
+
+            Process.Start("CMD.exe", command);
+        }
+    }
+}
